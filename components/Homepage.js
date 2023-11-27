f61bcb5b89a67b2fa3a69569b6717ac2ca35cd41
Khizar3333 <khizarahmad3333@gmail.com>
2023-11-26 20:17:13 -0800

    improve responsiveiness

--- components/Homepage.js
+++ components/Homepage.js
@@ -7,7 +7,7 @@ import {MdExpandMore} from 'react-icons/md'
 import Link from 'next/link'
 const Homepage = () => {
   return (
-    <div className=" h-screen  bg-gray-200 w-max sm:w-full text-center ">
+    <div className=" h-screen  bg-gray-200  sm:w-full text-center w-auto">
       <div className="max-w-screen-xl h-full w-full pt-24  p-4 flex flex-col items-center mx-auto ">
         <div className="w-64 h-64  overflow-hidden rounded-lg  mx-auto">
           <Image src={myimage}  alt='me'/>
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='w-max'>
          <Homepage/>
          <Portfolio/>
          <Experience/>
